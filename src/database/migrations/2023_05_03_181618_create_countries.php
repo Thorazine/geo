3fd5417be2be6b1ed1cb5089afde733e65fc80ba
Matthijs Openneer <matthijs.openneer@gmail.com>
2023-06-04 14:28:33 +0200

    removed timezones

--- src/database/migrations/2023_05_03_181618_create_countries.php
+++ src/database/migrations/2023_05_03_181618_create_countries.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->boolean('is_user_altered')->default(false);
             $table->integer('priority')->default(0);
 
-            $table->timestampsTz();
+            $table->timestamps();
         });
     }
 
